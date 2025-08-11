3ca48af21cd6a513004c08192837ba908c9bbd9b
Henry Alejandro Rivera Mendez <henryriveramendez@gmail.com>
2025-08-11 10:57:47 -0400

    Poniendo en la posicion actual de la parrilla de f1

--- src/app/data/pilotos.ts
+++ src/app/data/pilotos.ts
@@ -1,4 +1,123 @@
 export const pilotos = [
+    {
+    slug: 'piastri',
+    nombre: 'Oscar Piastri',
+    numero: 81,
+    color: 'rgb(255, 128, 0)', // para el estilo del número o bandera
+    equipo: 'McLaren',
+    pais: 'Australia',
+    escudo: 'assets/images/logos/mclaren-logo-iphone-12-pro-max.jpg',
+    imagen: 'assets/images/drivers-page/piastri.avif',
+    estadisticas: {
+      podios: 18,
+      puntos: 597,
+      gps: 56,
+      campeonatos: 0
+    }
+  },
+
+    {
+    slug: 'norris',
+    nombre: 'Lando Norris',
+    numero: 4,
+    color: 'rgb(255, 128, 0)', // para el estilo del número o bandera
+    equipo: 'McLaren',
+    pais: 'United Kingdom',
+    escudo: 'assets/images/logos/mclaren-logo-iphone-12-pro-max.jpg',
+    imagen: 'assets/images/drivers-page/norris.avif',
+    estadisticas: {
+      podios: 34,
+      puntos: 1183,
+      gps: 138,
+      campeonatos: 0
+    }
+  },
+
+    {
+    slug: 'verstappen',
+    nombre: 'Max Verstappen',
+    numero: 1,
+    color: 'rgb(227, 1, 24)',
+    equipo: 'Red Bull',
+    pais: 'Netherlands',
+    escudo: 'assets/images/logos/red-bull-f1-logo-j4y1l9zon2qh8604.jpg',
+    imagen: 'assets/images/drivers-page/verstappen.avif',
+    estadisticas: {
+      podios: 117,
+      puntos: 3178.5,
+      gps: 219,
+      campeonatos: 4
+    }
+  },
+
+    {
+    slug: 'russell',
+    nombre: 'George Russell',
+    numero: 63,
+    color: 'rgb(39, 244, 210)', // para el estilo del número o bandera
+    equipo: 'Mercedes',
+    pais: 'United Kingdom',
+    escudo: 'assets/images/logos/mer.jpg',
+    imagen: 'assets/images/drivers-page/russell.avif',
+    estadisticas: {
+      podios: 20,
+      puntos: 850,
+      gps: 138,
+      campeonatos: 0
+    }
+  },
+
+    {
+    slug: 'leclerc',
+    nombre: 'Charles Leclerc',
+    numero: 16,
+    color: 'rgb(232, 0, 32)', // para el estilo del número o bandera
+    equipo: 'Ferrari',
+    pais: 'Monaco',
+    escudo: 'assets/images/logos/ferrari.webp',
+    imagen: 'assets/images/drivers-page/leclerc.avif',
+    estadisticas: {
+      podios: 46,
+      puntos: 1534,
+      gps: 157,
+      campeonatos: 0
+    }
+  },
+
+    {
+    slug: 'hamilton',
+    nombre: 'Lewis Hamilton',
+    numero: 44,
+    color: 'rgb(232, 0, 32)', // para el estilo del número o bandera
+    equipo: 'Ferrari',
+    pais: 'United Kingdom',
+    escudo: 'assets/images/logos/ferrari.webp',
+    imagen: 'assets/images/drivers-page/hamilton.avif',
+    estadisticas: {
+      podios: 202,
+      puntos: 4941.5,
+      gps: 366,
+      campeonatos: 7
+    }
+  },
+
+    {
+    slug: 'antonelli',
+    nombre: 'Kimi Antonelli',
+    numero: 12,
+    color: 'rgb(39, 244, 210)', // para el estilo del número o bandera
+    equipo: 'Mercedes',
+    pais: 'Italy',
+    escudo: 'assets/images/logos/mer.jpg',
+    imagen: 'assets/images/drivers-page/antonelli.avif',
+    estadisticas: {
+      podios: 1,
+      puntos: 63,
+      gps: 10,
+      campeonatos: 0
+    }
+  },
+
   {
     slug: 'albon',
     nombre: 'Alexander Albon',
@@ -12,6 +131,40 @@ export const pilotos = [
       podios: 2,
       puntos: 282,
       gps: 114,
+      campeonatos: 0
+    }
+  },
+
+    {
+    slug: 'hulken',
+    nombre: 'Nico Hulken',
+    numero: 27,
+    color: 'rgb(82, 226, 82)', // para el estilo del número o bandera
+    equipo: 'Kick Sauber',
+    pais: 'Germany',
+    escudo: 'assets/images/logos/Stake F1.jpg',
+    imagen: 'assets/images/drivers-page/hulkenberg.avif',
+    estadisticas: {
+      podios: 0,
+      puntos: 591,
+      gps: 237,
+      campeonatos: 0
+    }
+  },
+
+    {
+    slug: 'ocon',
+    nombre: 'Esteban Ocon',
+    numero: 31,
+    color: 'rgb(235, 10, 30)', // para el estilo del número o bandera
+    equipo: 'Haas',
+    pais: 'France',
+    escudo: 'assets/images/logos/haas-f1-team-wl-1242x2668.jpg',
+    imagen: 'assets/images/drivers-page/ocon.avif',
+    estadisticas: {
+      podios: 4,
+      puntos: 467,
+      gps: 166,
       campeonatos: 0
     }
   },
@@ -33,92 +186,24 @@ export const pilotos = [
     }
   },
 
-  {
-    slug: 'bearman',
-    nombre: 'Oliver Bearman',
-    numero: 87,
-    color: 'rgb(0, 0, 0)', // para el estilo del número o bandera
-    equipo: 'Haas',
-    pais: 'United Kingdom',
-    escudo: 'assets/images/logos/haas-f1-team-wl-1242x2668.jpg',
-    imagen: 'assets/images/drivers-page/bearman.avif',
+    {
+    slug: 'stroll',
+    nombre: 'Lance Stroll',
+    numero: 18,
+    color: 'rgb(34, 153, 113)', // para el estilo del número o bandera
+    equipo: 'Aston Martin',
+    pais: 'Canada',
+    escudo: 'assets/images/logos/ASTON MARTIN LOGO WALLPAPER.jpg',
+    imagen: 'assets/images/drivers-page/stroll.avif',
     estadisticas: {
-      podios: 0,
-      puntos: 13,
-      gps: 13,
+      podios: 3,
+      puntos: 306,
+      gps: 176,
       campeonatos: 0
     }
   },
 
-  {
-    slug: 'colapinto',
-    nombre: 'Franco Colapinto',
-    numero: 43,
-    color: 'rgb(0, 0, 0)', // para el estilo del número o bandera
-    equipo: 'Alpine',
-    pais: 'Australia',
-    escudo: 'assets/images/logos/alpine-f1-logo-minimal-r3-1280x2120.jpg',
-    imagen: 'assets/images/drivers-page/colapinto.avif',
-    estadisticas: {
-      podios: 0,
-      puntos: 5,
-      gps: 13,
-      campeonatos: 0
-    }
-  },
-
-  {
-    slug: 'bortoleto',
-    nombre: 'Gabriel Bortoleto',
-    numero: 5,
-    color: 'rgb(82, 226, 82)', // para el estilo del número o bandera
-    equipo: 'Kick Sauber',
-    pais: 'Brazil',
-    escudo: 'assets/images/logos/Stake F1.jpg',
-    imagen: 'assets/images/drivers-page/bortoleto.avif',
-    estadisticas: {
-      podios: 0,
-      puntos: 0,
-      gps: 10,
-      campeonatos: 0
-    }
-  },
-
-  {
-    slug: 'gasly',
-    nombre: 'Pierre Gasly',
-    numero: 5,
-    color: 'rgb(242, 130, 180)', // para el estilo del número o bandera
-    equipo: 'Kick Sauber',
-    pais: 'Brazil',
-    escudo: 'assets/images/logos/alpine-f1-logo-minimal-r3-1280x2120.jpg',
-    imagen: 'assets/images/drivers-page/gasly.avif',
-    estadisticas: {
-      podios: 5,
-      puntos: 447,
-      gps: 163,
-      campeonatos: 0
-    }
-  },
-
-  {
-    slug: 'hamilton',
-    nombre: 'Lewis Hamilton',
-    numero: 44,
-    color: 'rgb(232, 0, 32)', // para el estilo del número o bandera
-    equipo: 'Ferrari',
-    pais: 'United Kingdom',
-    escudo: 'assets/images/logos/ferrari.webp',
-    imagen: 'assets/images/drivers-page/hamilton.avif',
-    estadisticas: {
-      podios: 202,
-      puntos: 4941.5,
-      gps: 366,
-      campeonatos: 7
-    }
-  },
-
-  {
+    {
     slug: 'hadjar',
     nombre: 'Isack Hadjar',
     numero: 6,
@@ -135,56 +220,23 @@ export const pilotos = [
     }
   },
 
-  {
-    slug: 'antonelli',
-    nombre: 'Kimi Antonelli',
-    numero: 12,
-    color: 'rgb(39, 244, 210)', // para el estilo del número o bandera
-    equipo: 'Mercedes',
-    pais: 'Italy',
-    escudo: 'assets/images/logos/mer.jpg',
-    imagen: 'assets/images/drivers-page/antonelli.avif',
+    {
+    slug: 'gasly',
+    nombre: 'Pierre Gasly',
+    numero: 5,
+    color: 'rgb(242, 130, 180)', // para el estilo del número o bandera
+    equipo: 'Kick Sauber',
+    pais: 'Brazil',
+    escudo: 'assets/images/logos/alpine-f1-logo-minimal-r3-1280x2120.jpg',
+    imagen: 'assets/images/drivers-page/gasly.avif',
     estadisticas: {
-      podios: 1,
-      puntos: 63,
-      gps: 10,
+      podios: 5,
+      puntos: 447,
+      gps: 163,
       campeonatos: 0
     }
   },
 
-  {
-    slug: 'norris',
-    nombre: 'Lando Norris',
-    numero: 4,
-    color: 'rgb(255, 128, 0)', // para el estilo del número o bandera
-    equipo: 'McLaren',
-    pais: 'United Kingdom',
-    escudo: 'assets/images/logos/mclaren-logo-iphone-12-pro-max.jpg',
-    imagen: 'assets/images/drivers-page/norris.avif',
-    estadisticas: {
-      podios: 34,
-      puntos: 1183,
-      gps: 138,
-      campeonatos: 0
-    }
-  },
-
-  {
-    slug: 'leclerc',
-    nombre: 'Charles Leclerc',
-    numero: 16,
-    color: 'rgb(232, 0, 32)', // para el estilo del número o bandera
-    equipo: 'Ferrari',
-    pais: 'Monaco',
-    escudo: 'assets/images/logos/ferrari.webp',
-    imagen: 'assets/images/drivers-page/leclerc.avif',
-    estadisticas: {
-      podios: 46,
-      puntos: 1534,
-      gps: 157,
-      campeonatos: 0
-    }
-  },
 
   {
     slug: 'lawson',
@@ -203,75 +255,7 @@ export const pilotos = [
     }
   },
 
-  {
-    slug: 'hulken',
-    nombre: 'Nico Hulken',
-    numero: 27,
-    color: 'rgb(82, 226, 82)', // para el estilo del número o bandera
-    equipo: 'Kick Sauber',
-    pais: 'Germany',
-    escudo: 'assets/images/logos/Stake F1.jpg',
-    imagen: 'assets/images/drivers-page/hulkenberg.avif',
-    estadisticas: {
-      podios: 0,
-      puntos: 591,
-      gps: 237,
-      campeonatos: 0
-    }
-  },
-
-  {
-    slug: 'ocon',
-    nombre: 'Esteban Ocon',
-    numero: 31,
-    color: 'rgb(235, 10, 30)', // para el estilo del número o bandera
-    equipo: 'Haas',
-    pais: 'France',
-    escudo: 'assets/images/logos/haas-f1-team-wl-1242x2668.jpg',
-    imagen: 'assets/images/drivers-page/ocon.avif',
-    estadisticas: {
-      podios: 4,
-      puntos: 467,
-      gps: 166,
-      campeonatos: 0
-    }
-  },
-
-  {
-    slug: 'piastri',
-    nombre: 'Oscar Piastri',
-    numero: 81,
-    color: 'rgb(255, 128, 0)', // para el estilo del número o bandera
-    equipo: 'McLaren',
-    pais: 'Australia',
-    escudo: 'assets/images/logos/mclaren-logo-iphone-12-pro-max.jpg',
-    imagen: 'assets/images/drivers-page/piastri.avif',
-    estadisticas: {
-      podios: 18,
-      puntos: 597,
-      gps: 56,
-      campeonatos: 0
-    }
-  },
-
-  {
-    slug: 'russell',
-    nombre: 'George Russell',
-    numero: 63,
-    color: 'rgb(39, 244, 210)', // para el estilo del número o bandera
-    equipo: 'Mercedes',
-    pais: 'United Kingdom',
-    escudo: 'assets/images/logos/mer.jpg',
-    imagen: 'assets/images/drivers-page/russell.avif',
-    estadisticas: {
-      podios: 20,
-      puntos: 850,
-      gps: 138,
-      campeonatos: 0
-    }
-  },
-
-  {
+    {
     slug: 'sainz',
     nombre: 'Carlos Sainz',
     numero: 55,
@@ -288,41 +272,24 @@ export const pilotos = [
     }
   },
 
-  {
-    slug: 'stroll',
-    nombre: 'Lance Stroll',
-    numero: 18,
-    color: 'rgb(34, 153, 113)', // para el estilo del número o bandera
-    equipo: 'Aston Martin',
-    pais: 'Canada',
-    escudo: 'assets/images/logos/ASTON MARTIN LOGO WALLPAPER.jpg',
-    imagen: 'assets/images/drivers-page/stroll.avif',
+    {
+    slug: 'bortoleto',
+    nombre: 'Gabriel Bortoleto',
+    numero: 5,
+    color: 'rgb(82, 226, 82)', // para el estilo del número o bandera
+    equipo: 'Kick Sauber',
+    pais: 'Brazil',
+    escudo: 'assets/images/logos/Stake F1.jpg',
+    imagen: 'assets/images/drivers-page/bortoleto.avif',
     estadisticas: {
-      podios: 3,
-      puntos: 306,
-      gps: 176,
+      podios: 0,
+      puntos: 0,
+      gps: 10,
       campeonatos: 0
     }
   },
 
-  {
-    slug: 'verstappen',
-    nombre: 'Max Verstappen',
-    numero: 1,
-    color: 'rgb(227, 1, 24)',
-    equipo: 'Red Bull',
-    pais: 'Netherlands',
-    escudo: 'assets/images/logos/red-bull-f1-logo-j4y1l9zon2qh8604.jpg',
-    imagen: 'assets/images/drivers-page/verstappen.avif',
-    estadisticas: {
-      podios: 117,
-      puntos: 3178.5,
-      gps: 219,
-      campeonatos: 4
-    }
-  },
-
-    {
+      {
     slug: 'yuki',
     nombre: 'Yuki Tsunoda',
     numero: 22,
@@ -338,6 +305,42 @@ export const pilotos = [
       campeonatos: 0
     }
   },
+
+  {
+    slug: 'bearman',
+    nombre: 'Oliver Bearman',
+    numero: 87,
+    color: 'rgb(0, 0, 0)', // para el estilo del número o bandera
+    equipo: 'Haas',
+    pais: 'United Kingdom',
+    escudo: 'assets/images/logos/haas-f1-team-wl-1242x2668.jpg',
+    imagen: 'assets/images/drivers-page/bearman.avif',
+    estadisticas: {
+      podios: 0,
+      puntos: 13,
+      gps: 13,
+      campeonatos: 0
+    }
+  },
+
+
+
+  {
+    slug: 'colapinto',
+    nombre: 'Franco Colapinto',
+    numero: 43,
+    color: 'rgb(0, 0, 0)', // para el estilo del número o bandera
+    equipo: 'Alpine',
+    pais: 'Australia',
+    escudo: 'assets/images/logos/alpine-f1-logo-minimal-r3-1280x2120.jpg',
+    imagen: 'assets/images/drivers-page/colapinto.avif',
+    estadisticas: {
+      podios: 0,
+      puntos: 5,
+      gps: 13,
+      campeonatos: 0
+    }
+  }
 ]
 
 export const dropdownPilotos = [
@@ -346,19 +349,20 @@ export const dropdownPilotos = [
   { slug: 'verstappen', nombre: 'Max Verstappen', imagen: 'assets/images/drivers/maxver01.avif' },
   { slug: 'russell', nombre: 'George Russell', imagen: 'assets/images/drivers/georus01.avif' },
   { slug: 'leclerc', nombre: 'Charles Leclerc', imagen: 'assets/images/drivers/chalec01.avif' },
-  { slug: 'antonelli', nombre: 'Kimi Antonelli', imagen: 'assets/images/drivers/andant01.avif' },
   { slug: 'hamilton', nombre: 'Lewis Hamilton', imagen: 'assets/images/drivers/lewham01.avif' },
+  { slug: 'antonelli', nombre: 'Kimi Antonelli', imagen: 'assets/images/drivers/andant01.avif' },
   { slug: 'albon', nombre: 'Alexander Albon', imagen: 'assets/images/drivers/alealb01.avif' },
-  { slug: 'ocon', nombre: 'Esteban Ocon', imagen: 'assets/images/drivers/estoco01.avif' },
-  { slug: 'stroll', nombre: 'Lance Stroll', imagen: 'assets/images/drivers/lanstr01.avif' },
-  { slug: 'gasly', nombre: 'Pierre Gasly', imagen: 'assets/images/drivers/piegas01.avif' },
   { slug: 'hulken', nombre: 'Nico Hulken', imagen: 'assets/images/drivers/nichul01.avif' },
-  { slug: 'bearman', nombre: 'Oliver Bearman', imagen: 'assets/images/drivers/olibea01.avif' },
+  { slug: 'ocon', nombre: 'Esteban Ocon', imagen: 'assets/images/drivers/estoco01.avif' },
+  { slug: 'alonso', nombre: 'Fernando Alonso', imagen: 'assets/images/drivers/feralo01.avif' },
+  { slug: 'stroll', nombre: 'Lance Stroll', imagen: 'assets/images/drivers/lanstr01.avif' },
   { slug: 'hadjar', nombre: 'Isack Hadjar', imagen: 'assets/images/drivers/isahad01.avif' },
+  { slug: 'gasly', nombre: 'Pierre Gasly', imagen: 'assets/images/drivers/piegas01.avif' },
+  { slug: 'lawson', nombre: 'Liam Lawson', imagen: 'assets/images/drivers/lialaw01.avif' },
+  { slug: 'bortoleto', nombre: 'Gabriel Bortoleto', imagen: 'assets/images/drivers/gabbor01.avif' },
   { slug: 'sainz', nombre: 'Carlos Sainz', imagen: 'assets/images/drivers/carsai01.avif' },
   { slug: 'yuki', nombre: 'Yuki Tsunoda', imagen: 'assets/images/drivers/yuktsu01.avif' },
-  { slug: 'alonso', nombre: 'Fernando Alonso', imagen: 'assets/images/drivers/feralo01.avif' },
-  { slug: 'lawson', nombre: 'Liam Lawson', imagen: 'assets/images/drivers/lialaw01.avif' },
+  { slug: 'bearman', nombre: 'Oliver Bearman', imagen: 'assets/images/drivers/olibea01.avif' },
   { slug: 'colapinto', nombre: 'Franco Colapinto', imagen: 'assets/images/drivers/fracol01.png.avif' },
-  { slug: 'bortoleto', nombre: 'Gabriel Bortoleto', imagen: 'assets/images/drivers/gabbor01.avif' },
+
 ];
